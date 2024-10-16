3455dafcf3c966b64d939814b584c6d3666b201e
Ines-sanz <inessanzm@hotmail.es>
2024-10-16 08:45:16 +0000

    fix: JS chhanges

--- script.js
+++ script.js
@@ -1,50 +1,48 @@
 //variables
-const carta = document.querySelector('#num')
 const paloUp = document.querySelector('#paloUp')
 const paloDown = document.querySelector('#paloDown')
-const newCardButton = document.querySelector('#newCard')
+const card = document.querySelector('#num')
+const newCard = document.querySelector('#newCard')
 
 let palos = ['♦', '♥', '♠', '♣']
-let numeros = ['2','3','4','5','6','7','8','9','10','J','K','Q']
+let numeros = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'K', 'Q']
 
 //funciones
-
-function getPalo (){
-    let randomIndex = Math.floor(Math.random()* palos.length)
-    return palos[randomIndex]
-}
-console.log(getPalo())
-
-function getNumber (){
-    let randomIndex = Math.floor(Math.random()* numeros.length)
-    return numeros[randomIndex]
-}
-console.log(getNumber())
-
-function getRandomCard(){
-let numero = getNumber();
-let palo = getPalo();
-
-paloUp.innerText = palo;
-carta.innerText = numero;
-paloDown.innerText = palo;
-
-if(palo == "♥" || palo == "♦"){
-    paloUp.style.color = 'red';
-    paloDown.style.color = 'red';
-    carta.style.color = 'red';
+function getNumber() {
+    let random = Math.floor(Math.random() * numeros.length)
+    return numeros[random]
 }
 
-else{
-    paloUp.style.color = 'black';
-    paloDown.style.color = 'black';
-    carta.style.color = 'black';
+function getPalo() {
+    let random = Math.floor(Math.random() * palos.length)
+    return palos[random]
 }
 
+function getCard() {
+    let num = getNumber()
+    let palo = getPalo()
+
+    paloUp.innerText = palo
+    card.innerText = num
+    paloDown.innerText = palo
+
+    if (palo == '♥' || palo == '♦') {
+        paloUp.style.color = 'red'
+        card.style.color = 'red'
+        paloDown.style.color = 'red'
+    }
+
+    else {
+        paloUp.style.color = 'black'
+        card.style.color = 'black'
+        paloDown.style.color = 'black'
+    }
+
 }
+
 
 //window.onload = getRandomCard;
 
 //eventos
 
-newCardButton.addEventListener('click', getRandomCard);
+newCard.addEventListener('click', getCard)
